bab4b4b81ff372f85112af333aaa687adafff93f
Vishal-mahale <vmahale799@gmail.com>
2026-04-21 11:37:25 +0530

    Project is created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return <>
+   <div className="text-center font-bold mt-20">This is Expense Tracker</div>
+  </>;
+}
+
+export default App;
